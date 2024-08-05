3f7ada91b00b63bab5e690fdee31e23d2aacad11
xmh0511 <970252187@qq.com>
2024-08-05 16:24:45 +0800

    u

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::net::SocketAddrV4;
+use std::{net::SocketAddrV4, sync::{atomic::{AtomicBool, Ordering}, Arc}};
+use std::sync::mpsc;
+use ctrlc2;
 
 use p2p_channel::{channel::Route, punch::NatType};
 fn main() {
@@ -21,39 +23,57 @@ fn main() {
         )
         .unwrap();
     println!("nat info: {:?}", nat);
-    {
+	let (tx, rx) = mpsc::channel();
+	let handle = ctrlc2::set_handler(move || {tx.send(()).expect("Could not send signal on channel."); true})
+	.expect("Error setting Ctrl-C handler");
+	let terminate = Arc::new(AtomicBool::new(false));
+	let terminate1 = terminate.clone();
+	let terminate2 = terminate.clone();
+	let terminate3 = terminate.clone();
+	let terminate4 = terminate.clone();
+	let terminate5 = terminate.clone();
+    let t1 = {
         // 空闲处理，添加的路由空闲时触发
         std::thread::spawn(move || {
             loop {
+				if terminate1.load(Ordering::Relaxed){
+					break;
+				}
                 let (idle_status, id, _route) = idle.next_idle().unwrap();
                 // channel.send_to_route()
                 //channel.remove_route(&id[0]);
                 println!("idle {:?} {}", idle_status, &id[0]);
             }
-        });
-    }
-    {
+        })
+    };
+    let (t2,t3) = {
         let ident = args[1].clone();
         let ident2 = ident.clone();
         // 打洞处理
         let mut punch2 = punch.try_clone().unwrap();
-        std::thread::spawn(move || {
+        (std::thread::spawn(move || {
             let buf = format!("hello from {}", ident);
             loop {
+				if terminate2.load(Ordering::Relaxed){
+					break;
+				}
                 let (id, nat_info) = punch.next_cone(None).unwrap();
                 println!("{id} -> {nat_info:?}");
                 punch.punch(buf.as_bytes(), id, nat_info).unwrap();
             }
-        });
+        }),
         std::thread::spawn(move || {
             let buf = format!("hello from {}", ident2);
             loop {
+				if terminate3.load(Ordering::Relaxed){
+					break;
+				}
                 let (id, nat_info) = punch2.next_symmetric(None).unwrap();
                 println!("{id} -> {nat_info:?}");
                 punch2.punch(buf.as_bytes(), id, nat_info).unwrap();
             }
-        });
-    }
+        }))
+    };
 
     let mut buf = [0; 1500];
 
@@ -70,9 +90,13 @@ fn main() {
     let id = format!("{s}");
     // Do something...
     let chanel2 = channel.try_clone().unwrap();
+	let chanel3 = channel.try_clone().unwrap();
     let id2 = id.clone();
-    std::thread::spawn(move || {
+    let t4 = std::thread::spawn(move || {
         loop {
+			if terminate4.load(Ordering::Relaxed){
+				break;
+			}
             chanel2
                 .punch(
                     id2.clone(),
@@ -103,20 +127,35 @@ fn main() {
         }
     });
 
-    let mut status = false;
-    // 接收数据处理
-    loop {
-        let (len, route_key) = channel.recv_from(&mut buf, None).unwrap();
-        let text = String::from_utf8_lossy(&buf[..len]).to_string();
-        println!("receive {text} {route_key:?}");
-        channel.add_route(id.clone(), Route::from(route_key, 10, 64)); //超时触发空闲
-        if !status {
-            let msg = format!("my name is {}", args[1]);
-            //channel.send_to_route(msg.as_bytes(), &route_key).unwrap();
-            //println!("route table {:?}", channel.route_table());
-            _ = channel.send_to_id(msg.as_bytes(), &id).unwrap();
-            //channel.send_to_addr(msg.as_bytes(), addr.parse().unwrap()).unwrap();
-            status = true;
-        }
-    }
+	let t5 = std::thread::spawn(move ||{
+		let mut status = false;
+		// 接收数据处理
+		loop {
+			if terminate5.load(Ordering::Relaxed){
+				break;
+			}
+			let (len, route_key) = channel.recv_from(&mut buf, None).unwrap();
+			let text = String::from_utf8_lossy(&buf[..len]).to_string();
+			println!("receive {text} {route_key:?}");
+			channel.add_route(id.clone(), Route::from(route_key, 10, 64)); //超时触发空闲
+			if !status {
+				let msg = format!("my name is {}", args[1]);
+				//channel.send_to_route(msg.as_bytes(), &route_key).unwrap();
+				//println!("route table {:?}", channel.route_table());
+				_ = channel.send_to_id(msg.as_bytes(), &id).unwrap();
+				//channel.send_to_addr(msg.as_bytes(), addr.parse().unwrap()).unwrap();
+				status = true;
+			}
+		}
+	});
+	rx.recv().expect("Could not receive from channel.");
+	println!("exit");
+	chanel3.close().unwrap();
+	terminate.store(true, Ordering::Relaxed);
+	t1.join().unwrap();
+	t2.join().unwrap();
+	t3.join().unwrap();
+	t4.join().unwrap();
+	t5.join().unwrap();
+	handle.join().unwrap();
 }
